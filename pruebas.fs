81b3bcc0ae3b55b4925a3bc294e9c3165ab878ce
Rodrigo Tucuman <rembert.rodrigo@gmail.com>
2020-07-14 12:45:13 -0400

    tuplas

--- pruebas.fs
+++ pruebas.fs
@@ -1,14 +1,11 @@
-module NumerosCadenasBooleanos = 
-    /// integer.
-    let ejemploDeEntero = 176
-    /// Cadenas usan comillas dobles.
-    let cadena = "Hello"
-    /// Valores booleanos 'true' and 'false'.
-    let boolean1 = true
-    let boolean2 = false
-    ///impresiones en pantalla
-    printfn "el numero es: %d" ejemploDeEntero
-    printfn "la cadena es: %s" cadena
-    printfn "los valores booleanos son: %b y %b" boolean1 boolean2 
-
-    
+module Tuplas =
+    /// una tupla simple de integers.
+    let tupla1 = (1, 2, 3)
+    /// a continuacion se crea una función que intercambia el orden de dos valores en una tupla. 
+    /// la inferencia de tipos de F# generalizará automáticamente la función para tener un tipo genérico, 
+    /// lo que significa que funcionará con cualquier tipo
+    let intercambiarElems (a, b) = (b, a)
+    printfn "El resultado del intercambio (1, 2) es %A" (intercambiarElems (1,2))
+    /// una tupla que consiste de un entero, cadena y double,
+    let tupla2 = (1, "fred", 3.1415)
+    printfn "tupla1: %A\ttupla2: %A" tupla1 tupla2
