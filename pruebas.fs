272322a2548b192cbc99390e37ce2d9b920bcb8a
Rodrigo Tucuman <rembert.rodrigo@gmail.com>
2020-07-16 11:05:47 -0400

    bucle for...to

--- pruebas.fs
+++ pruebas.fs
@@ -1,11 +1,10 @@
-// Este ejemplo utiliza patrones que tienen cuando los guardias.
-let detectarValor punto objetivo =
-    match punto with
-    | (a, b) when a = objetivo && b = objetivo -> printfn "Ambos valores coinciden con el objetivo %d." objetivo
-    | (a, b) when a = objetivo -> printfn "El primer valor coincide con el objetivo (%d, %d)" objetivo b
-    | (a, b) when b = objetivo -> printfn "El segundo valor coincidió con el objetivo (%d, %d)" a objetivo
-    | _ -> printfn "Ninguno de los dos valores coincide con el objetivo."
-detectarValor (0, 0) 0
-detectarValor (1, 0) 0
-detectarValor (0, 10) 0
-detectarValor (10, 15) 0
+// Un bucle for... to que utiliza funciones como las expresiones de inicio y fin.
+let inicio x y = x - 2*y
+let fin x y = x + 2*y
+
+let funcion x y =
+  for i = (inicio x y) to (fin x y) do
+     printf "%d " i
+  printfn ""
+
+funcion 10 4
