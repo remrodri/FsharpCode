8f88cc97500e2b40eda3ec1529877472298b30d7
Rodrigo Tucuman <rembert.rodrigo@gmail.com>
2020-07-19 00:44:16 -0400

    recursividad

--- pruebas.fs
+++ pruebas.fs
@@ -1,12 +1,13 @@
-exception Error1 of string
-// Usando un tipo de tupla como tipo de argumento.
-exception Error2 of string * int
-let function1 x y =
-   try
-      if x = y then raise (Error1("x"))
-      else raise (Error2("x", 10))
-   with
-      | Error1(str) -> printfn "Error1 %s" str
-      | Error2(str, i) -> printfn "Error2 %s %d" str i
-function1 10 10
-function1 9 2
+// En el código siguiente se muestra una función recursiva que calcula el númerode Fibonacci n.
+module recursividad = 
+    let rec fib n =
+        if n <= 2 then 1
+        else fib (n - 1) + fib (n - 2)
+//Funciones mutuamente recursivas
+//A veces,las funciones son mutuamente recursivas, lo que significa que las llamadas forman un círculo, donde una función llama a otra que, a su vez, llama a la primera, con cualquier número de llamadas entre. Debe definir estas funciones juntas en un let enlace, utilizando la and palabra clave para vincularlas.
+let rec Par x = 
+    if x = 0 then true 
+    else Impar (x-1) 
+and Impar x = 
+    if x = 0 then false 
+    else Par (x-1)
